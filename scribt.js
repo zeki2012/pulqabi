0cb9905939c3e4c7864dee18d4dc04f4c312dc26
= <=>
2023-10-07 11:55:03 +0400

    first commit

---
+++ scribt.js
@@ -0,0 +1,9 @@
+const pulqabi = []
+
+pulqabi[0] = +prompt('pul at')
+pulqabi[1] = +prompt('pul at')
+pulqabi[2] = +prompt('pul at')
+
+let qargidali = pulqabi[0] + pulqabi[1] + pulqabi[2];
+
+document.getElementById('zekisko').innerHTML = 'Sizin pul qabinizda ' + qargidali + ' AZN yigilib'
